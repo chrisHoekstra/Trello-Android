bde8390a73a6a0eddcda1106f0d224f966fe7869
Chris Hoekstra <chris.hoekstra@t8webware.com>
2011-11-14 09:50:10 -0600

    Project setup

---
+++ src/com/ch/trello/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ch.trello.activity;
+
+import com.ch.trello.R;
+import com.ch.trello.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
