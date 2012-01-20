401cf5c4db74bf9d0dc6de57bfad176c19b5a87f
Chris Hoekstra <chris.hoekstra@t8webware.com>
2012-01-20 14:29:07 -0600

    LabelVO

---
+++ src/com/chrishoekstra/trello/vo/LabelVO.java
@@ -0,0 +1,13 @@
+package com.chrishoekstra.trello.vo;
+
+public class LabelVO {
+    public static final String GREEN  = "green";
+    public static final String YELLOW = "yellow";
+    public static final String ORANGE = "orange";
+    public static final String RED    = "red";
+    public static final String PURPLE = "purple";
+    public static final String BLUE   = "blue";
+    
+    public String name;
+    public String color;
+}
